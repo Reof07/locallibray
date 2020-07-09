cfc4e15448c1024d60f3d34c470604a1d21e8697
Ramon Ojeda <reof1922@gmail.com>
2020-07-09 23:28:46 +0200

    se creo el modelo genero (Genre),se cargo la migracion a la DB, Se creo el superUser y se registro el modelo al panel admin

--- catalog/url.py
+++ catalog/url.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
-
+path('', views.home_Test),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home_Test(request):
+    return HttpResponse('hola')
+
